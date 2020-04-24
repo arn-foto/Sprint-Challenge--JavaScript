55b51dd7b1558b7282825edd25e8e0763f0ad1fa
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-04-24 12:07:49 -0700

    Add files via upload

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,31 +1,26 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-class CubeMaker{
-    constructor(attributes){
+class CubeMaker {
+    constructor(attributes) {
         this.length = attributes.length;
         this.width = attributes.width;
         this.height = attributes.height;
     }
-    volume(){
-       return this.length * this.width * this.height;  
+    volume() {
+        return this.length * this.width * this.height;
     }
-    surfaceArea(){
+    surfaceArea() {
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
 }
 
-
-
 const cuboid2 = new CubeMaker({
-  length : 4,
-  width: 5,
-  height:5,
+    length: 4,
+    width: 5,
+    height: 5,
 });
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid2.volume()); // 100
 console.log(cuboid2.surfaceArea()); // 130
-/* == Step 1: Base Constructor ==
-Create a constructor function named CuboidMaker that accepts properties for length, width, and height
-*/
 
 
 
